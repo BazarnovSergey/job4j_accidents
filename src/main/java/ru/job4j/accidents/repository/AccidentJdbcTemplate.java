8ff018bbd0d6b163e407de467de7e5809225b99d
BazarnovSergey <ssergey101@yandex.ru>
2023-01-25 01:16:13 +0300

    0. Spring DataSource [#6878 #384374]

--- src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
@@ -1,8 +1,8 @@
 package ru.job4j.accidents.repository;
 
 import lombok.AllArgsConstructor;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -24,21 +24,6 @@ public class AccidentJdbcTemplate implements AccidentRepository {
 
     private final static String INSERT_ACCIDENT = "insert into accident (name,description_accident,car_number, type_id,"
             + "address_accident,status_id,photo,date_accident) values (?,?,?,?,?,?,?,?)";
-
-    private final static String SELECT_ACCIDENT = "select * from accident";
-
-    private final static String SELECT_ACCIDENT_ID = "select * from accident where id = ?";
-
-    private final static String SELECT_ACCIDENT_TYPE_ID = "select at.id, at.name from accident_type at "
-            + "join accident a on at.id = a.type_id where a.id = ?";
-
-    private final static String SELECT_RULE_ID = "SELECT * FROM rule WHERE id = ?";
-
-    private final static String SELECT_ACCIDENT_STATUS_JOIN_ACCIDENT = "select ast.id, ast.status from"
-            + " accident_status as ast " + " join accident a on ast.id = a.status_id where a.id = ?";
-
-    private final static String SELECT_RULE_JOIN_ACCIDENT_RULE = "select * from rule as r join accident_rule ar "
-            + "on r.id = ar.rule_id where accident_id = ?;";
 
     private final static String UPDATE_ACCIDENT = "update accident set name = ?,description_accident = ?,car_number = ?,"
             + " type_id = ?, address_accident = ?,status_id = ?,photo = ?,date_accident = ? where id = ?";
@@ -87,56 +72,78 @@ public class AccidentJdbcTemplate implements AccidentRepository {
 
     @Override
     public Optional<Accident> findById(int id) {
-        return jdbc.queryForObject(SELECT_ACCIDENT_ID,
-                (rs, row) -> {
-                    Accident accident = new Accident();
-                    accident.setId(rs.getInt("id"));
-                    accident.setName(rs.getString("name"));
-                    accident.setText(rs.getString("description_accident"));
-                    accident.setCarNumber(rs.getString("car_number"));
-                    accident.setType(jdbc.query(SELECT_ACCIDENT_TYPE_ID,
-                                    new Object[]{rs.getInt("id")},
-                                    new BeanPropertyRowMapper<>(AccidentType.class))
-                            .stream().findAny().orElse(null));
-                    accident.setRules(new HashSet<>(jdbc.query(SELECT_RULE_JOIN_ACCIDENT_RULE,
-                            new Object[]{rs.getInt("id")},
-                            new BeanPropertyRowMapper<>(Rule.class))));
-                    accident.setAddress(rs.getString("address_accident"));
-                    accident.setAccidentStatus(jdbc.query(SELECT_ACCIDENT_STATUS_JOIN_ACCIDENT,
-                                    new Object[]{rs.getInt("id")},
-                                    new BeanPropertyRowMapper<>(AccidentStatus.class))
-                            .stream().findAny().orElse(null));
-                    accident.setPhoto(rs.getBytes("photo"));
-                    accident.setDateAccident(rs.getTimestamp("date_accident").toLocalDateTime());
-                    return Optional.of(accident);
-                },
-                id);
+        String sql = "select distinct ac.id, ac.name, "
+                + "ac.description_accident, "
+                + "ac.car_number ,"
+                + "ac.type_id, "
+                + "ac.address_accident, "
+                + "ac.status_id, "
+                + "ac.photo, "
+                + "ac.date_accident, "
+                + "at.id as type_id, "
+                + "at.name as type_name, "
+                + "ast.id as status_id, "
+                + "ast.status, "
+                + "r.id as rule_id, "
+                + "r.name as rule_name "
+                + "from accident ac "
+                + "join accident_type at on at.id = ac.type_id "
+                + "join accident_rule ar on ac.id = ar.accident_id "
+                + "join accident_status ast on ast.id = ac.status_id "
+                + "join rule r on r.id = ar.rule_id "
+                + "where ac.id = ?";
+        Map<Integer, Accident> accidentById = jdbc.query(sql, extractAccident, id);
+        return Optional.ofNullable(accidentById.get(id));
     }
 
     @Override
     public List<Accident> getAccidents() {
-        return jdbc.query(SELECT_ACCIDENT,
-                (rs, row) -> {
-                    Accident accident = new Accident();
-                    accident.setId(rs.getInt("id"));
-                    accident.setName(rs.getString("name"));
-                    accident.setText(rs.getString("description_accident"));
-                    accident.setCarNumber(rs.getString("car_number"));
-                    accident.setType(jdbc.query(SELECT_ACCIDENT_TYPE_ID,
-                                    new Object[]{rs.getInt("id")},
-                                    new BeanPropertyRowMapper<>(AccidentType.class))
-                            .stream().findAny().orElse(null));
-                    accident.setRules(new HashSet<>(jdbc.query(SELECT_RULE_ID,
-                            new Object[]{rs.getInt("id")},
-                            new BeanPropertyRowMapper<>(Rule.class))));
-                    accident.setAddress(rs.getString("address_accident"));
-                    accident.setAccidentStatus(jdbc.query(SELECT_ACCIDENT_STATUS_JOIN_ACCIDENT,
-                                    new Object[]{rs.getInt("id")},
-                                    new BeanPropertyRowMapper<>(AccidentStatus.class))
-                            .stream().findAny().orElse(null));
-                    accident.setPhoto(rs.getBytes("photo"));
-                    accident.setDateAccident(rs.getTimestamp("date_accident").toLocalDateTime());
-                    return accident;
-                });
+        String sql = "select distinct ac.id, ac.name, "
+                + "ac.description_accident, "
+                + "ac.car_number ,"
+                + "ac.type_id, "
+                + "ac.address_accident, "
+                + "ac.status_id, "
+                + "ac.photo, "
+                + "ac.date_accident, "
+                + "at.id as type_id, "
+                + "at.name as type_name, "
+                + "ast.id as status_id, "
+                + "ast.status, "
+                + "r.id as rule_id, "
+                + "r.name as rule_name "
+                + "from accident ac "
+                + "join accident_type at on at.id = ac.type_id "
+                + "join accident_rule ar on ac.id = ar.accident_id "
+                + "join accident_status ast on ast.id = ac.status_id "
+                + "join rule r on r.id = ar.rule_id";
+        Map<Integer, Accident> accidents = jdbc.query(sql, extractAccident);
+        assert accidents != null;
+        return new ArrayList<>(accidents.values());
     }
+
+    private final ResultSetExtractor<Map<Integer, Accident>> extractAccident = (rs) -> {
+        Map<Integer, Accident> data = new HashMap<>();
+        while (rs.next()) {
+            Accident accident = new Accident();
+            accident.setId(rs.getInt("id"));
+            accident.setName(rs.getString("name"));
+            accident.setText(rs.getString("description_accident"));
+            accident.setCarNumber(rs.getString("car_number"));
+            AccidentType type = new AccidentType();
+            type.setId(rs.getInt("type_id"));
+            type.setName(rs.getString("type_name"));
+            accident.setType(type);
+            accident.setAddress(rs.getString("address_accident"));
+            AccidentStatus status = new AccidentStatus();
+            status.setId(rs.getInt("status_id"));
+            status.setStatus(rs.getString("status"));
+            accident.setAccidentStatus(status);
+            accident.setPhoto(rs.getBytes("photo"));
+            accident.setDateAccident(rs.getTimestamp("date_accident").toLocalDateTime());
+            accident.setRules(new HashSet<>(Set.of(new Rule(rs.getInt("rule_id"), rs.getString("rule_name")))));
+            data.putIfAbsent(accident.getId(), accident);
+        }
+        return data;
+    };
 }
