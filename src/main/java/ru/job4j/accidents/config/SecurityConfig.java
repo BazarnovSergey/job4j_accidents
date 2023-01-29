0247e7f23bccce92ad1a567ac63ab1e134f2828a
BazarnovSergey <ssergey101@yandex.ru>
2023-01-29 12:41:34 +0300

    0. Spring Security [#6879 #385735]

--- src/main/java/ru/job4j/accidents/config/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.job4j.accidents.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,16 +14,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("123456")).roles("USER")
+                .passwordEncoder(passwordEncoder())
+                .withUser("user").password(passwordEncoder().encode("123456")).roles("USER")
                 .and()
-                .withUser("admin").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
+                .withUser("admin").password(passwordEncoder().encode("123456")).roles("USER", "ADMIN");
     }
 
     @Bean
