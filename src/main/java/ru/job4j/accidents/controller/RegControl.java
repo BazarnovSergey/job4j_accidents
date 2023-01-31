b031cdf2f8d870082e3d8abd02e7ef156de66a5b
BazarnovSergey <ssergey101@yandex.ru>
2023-01-31 21:15:26 +0300

    2. Регистрация пользователя [#296069 #386550]

--- src/main/java/ru/job4j/accidents/controller/RegControl.java
+++ src/main/java/ru/job4j/accidents/controller/RegControl.java
@@ -1,9 +1,8 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.Model;
 import ru.job4j.accidents.model.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.accidents.service.AuthorityService;
 import ru.job4j.accidents.service.UserService;
-import ru.job4j.accidents.util.UserValidator;
-
-import javax.validation.Valid;
 
 @Controller
 public class RegControl {
@@ -21,21 +17,21 @@ public class RegControl {
     private final PasswordEncoder encoder;
     private final UserService users;
     private final AuthorityService authorities;
-    private final UserValidator userValidator;
 
     @Autowired
     public RegControl(PasswordEncoder encoder, UserService users,
-                      AuthorityService authorities, UserValidator userValidator) {
+                      AuthorityService authorities) {
         this.encoder = encoder;
         this.users = users;
         this.authorities = authorities;
-        this.userValidator = userValidator;
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
-        userValidator.validate(user, bindingResult);
-        if (bindingResult.hasErrors()) {
+    public String regSave(Model model, @ModelAttribute("user") User user) {
+        String errorMessage = null;
+        if (users.getUser(user.getUsername()).isPresent()) {
+            errorMessage = "This name is already taken";
+            model.addAttribute("errorMessage", errorMessage);
             return "reg";
         }
         user.setEnabled(true);
@@ -46,7 +42,7 @@ public class RegControl {
     }
 
     @GetMapping("/reg")
-    public String regPage(@ModelAttribute("user") User user) {
+    public String regPage() {
         return "reg";
     }
 
