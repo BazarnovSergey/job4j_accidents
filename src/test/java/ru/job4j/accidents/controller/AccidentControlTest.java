ae2fae77ae547a4942e05c181b23232186bd662a
BazarnovSergey <ssergey101@yandex.ru>
2023-01-31 21:53:39 +0300

    0. Spring test [#6881]

---
+++ src/test/java/ru/job4j/accidents/controller/AccidentControlTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.accidents.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.Main;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+class AccidentControlTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void whenCreateAccidentThenReturnViewCreateAccident() throws Exception {
+        this.mockMvc.perform(get("/createAccident"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("createAccident"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenShowThenReturnViewShow() throws Exception {
+        this.mockMvc.perform(get("/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("show"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenUpdateThenReturnViewFormUpdateAccident() throws Exception {
+        this.mockMvc.perform(get("/1/formUpdateAccident"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("formUpdateAccident"));
+    }
+
+}
